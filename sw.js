4a2c4ff3388a09d23c0ca30f20d681eef91b8adc
Беляев Роман <belyaev.roman31@gmail.com>
2026-09-19 10:53:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE = 'otoplenie-svodka-v1';
+const PRECACHE = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-512-maskable.png',
+  './icons/icon-180.png',
+  './icons/favicon-32.png',
+  './images/dvuhtrubnaya_priv_nasos.jpg',
+  './images/leningradka_bypass.png',
+  './images/leningradka_gorizontalnaya.jpg',
+  './images/luchevaya_razvodka.jpg',
+  './images/luchevaya_shema_3_1.jpg',
+  './images/poputnaya_25950.gif',
+  './images/poputnaya_razvodka_05.jpg',
+  './images/tupikovaya_shemi_18_1.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then((cache) => cache.addAll(PRECACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() =>
+          caches.match(request).then((cached) => cached || caches.match('./index.html'))
+        )
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) return cached;
+      return fetch(request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(request, copy));
+          }
+          return response;
+        })
+        .catch(() => cached);
+    })
+  );
+});
